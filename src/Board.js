7adff696fd6e4d5b937b6f80e6e32b78a2c7ff85
hankyulkim93 <hankyulkim93@gmail.com>
2019-07-24 16:58:12 -0700

    passed all board tests

--- src/Board.js
+++ src/Board.js
@@ -172,13 +172,30 @@
     //
     // test if a specific minor diagonal on this board contains a conflict
     hasMinorDiagonalConflictAt: function(minorDiagonalColumnIndexAtFirstRow) {
-      return false; // fixme
+      var count = 0;
+      for (var i = 0; i < this.get('n'); i++) {
+        if (this.get(0 + i)[minorDiagonalColumnIndexAtFirstRow - i] === 1) {
+          count++;
+        }
+      }
+      if (count > 1) {
+        return true;
+      } else {
+        return false;
+      }
     },
 
     // test if any minor diagonals on this board contain conflicts
     hasAnyMinorDiagonalConflicts: function() {
-      return false; // fixme
-    }
+      var range = 2*(this.get('n')) - 3;
+      for (var i = 1; i <= range; i++) {
+        if (this.hasMinorDiagonalConflictAt(i) === true) {
+          return true;
+        } 
+      } 
+      return false;
+    },
+
 
     /*--------------------  End of Helper Functions  ---------------------*/
 
